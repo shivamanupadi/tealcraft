15747af4c3b251bebb13e5bc68cb319bb69c7988
shivamanupadi <137275428+shivamanupadi@users.noreply.github.com>
2024-01-05 00:43:42 +0530

    custom typings

--- apps/web/src/Components/Workspace/Contract/ContractEditor/ContractEditor.tsx
+++ apps/web/src/Components/Workspace/Contract/ContractEditor/ContractEditor.tsx
@@ -2,14 +2,36 @@ import { ReactElement, useRef } from "react";
 import "./ContractEditor.scss";
 import { RootState } from "../../../../Redux/store";
 import { useSelector } from "react-redux";
-import { Editor } from "@monaco-editor/react";
+import { Editor, Monaco } from "@monaco-editor/react";
+import { monacoLightTheme } from "./themes/light";
+// @ts-ignore
+import customTypings from "!!raw-loader!./typings/types/global.d.ts";
 
 function ContractEditor(): ReactElement {
   const { contract } = useSelector((state: RootState) => state.contract);
   const editorRef = useRef(null);
 
-  function editorMounted(editor: any) {
+  function editorMounted(editor: any, monaco: Monaco) {
     editorRef.current = editor;
+
+    monaco.editor.defineTheme("classic-theme", monacoLightTheme);
+    monaco.editor.setTheme("classic-theme");
+
+    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+      target: monaco.languages.typescript.ScriptTarget.ES2016,
+      allowNonTsExtensions: true,
+      moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+    });
+
+    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+      noSemanticValidation: true,
+      noSyntaxValidation: true,
+    });
+
+    monaco.languages.typescript.typescriptDefaults.addExtraLib(
+      customTypings,
+      "file:///node_modules/@algorandfoundation/tealscript/types/global.d.ts",
+    );
   }
 
   const source = contract?.name || "";
@@ -21,6 +43,19 @@ function ContractEditor(): ReactElement {
           width={"600px"}
           language="typescript"
           defaultValue={source}
+          theme="classic-theme"
+          options={{
+            fontSize: 13,
+            minimap: {
+              enabled: false,
+            },
+            tabSize: 8,
+            autoIndent: "advanced",
+            detectIndentation: false,
+            formatOnPaste: true,
+            formatOnType: true,
+            lineHeight: 1.2,
+          }}
           onMount={editorMounted}
         ></Editor>
       </div>
