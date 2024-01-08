395d732d1627bb01b6ac96fd2a42a75394e33e2b
shivamanupadi <137275428+shivamanupadi@users.noreply.github.com>
2024-01-08 13:09:21 +0530

    download app spec

--- apps/web/src/Components/Workspace/Contract/ContractConsole/ContractAppSpec/ContractAppSpec.tsx
+++ apps/web/src/Components/Workspace/Contract/ContractConsole/ContractAppSpec/ContractAppSpec.tsx
@@ -1,6 +1,8 @@
 import { ReactElement } from "react";
 import "./ContractAppSpec.scss";
 import ReactJson from "react-json-view";
+import { Button } from "@mui/material";
+import { downloadJson } from "@repo/utils";
 
 export type ContractAppSpecProps = {
   appSpec: any;
@@ -10,6 +12,19 @@ function ContractAppSpec({ appSpec }: ContractAppSpecProps): ReactElement {
   return (
     <div className="contract-app-spec-wrapper">
       <div className="contract-app-spec-container">
+        <div className="spec-actions">
+          <Button
+            color={"primary"}
+            onClick={() => {
+              downloadJson(appSpec, "application.json");
+            }}
+            variant={"contained"}
+            className="small-button"
+            size={"small"}
+          >
+            Download
+          </Button>
+        </div>
         <div className="app-spec">
           <ReactJson
             src={appSpec}
--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -1,3 +1,13 @@
 export function getExceptionMsg(e: any): string {
   return e?.response?.data?.message || e?.message;
 }
+
+export function downloadJson(data: JSON, fileName: string) {
+  const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+    JSON.stringify(data),
+  )}`;
+  const link = document.createElement("a");
+  link.href = jsonString;
+  link.download = fileName;
+  link.click();
+}
