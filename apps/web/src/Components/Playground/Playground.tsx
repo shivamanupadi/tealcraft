9af66cdff3b03a59466d30cb2ed367ec87880a92
shivamanupadi <137275428+shivamanupadi@users.noreply.github.com>
2024-02-20 15:51:13 +0530

    method executor

--- apps/web/src/Components/Playground/Playground.tsx
+++ apps/web/src/Components/Playground/Playground.tsx
@@ -5,24 +5,34 @@ import { Close } from "@mui/icons-material";
 import AccountPicker from "../AccountPicker/AccountPicker";
 import NodePicker from "../NodePicker/NodePicker";
 import { Button, FormLabel, Grid } from "@mui/material";
-import { createApp, mnemonicAccount } from "@algorandfoundation/algokit-utils";
+import {
+  callApp,
+  createApp,
+  mnemonicAccount,
+  decodeAppState,
+} from "@algorandfoundation/algokit-utils";
 import { useSelector } from "react-redux";
 import { RootState } from "../../Redux/store";
 import {
   A_ABI_METHOD_EXECUTOR_ARG,
+  convertExecutorArgsToMethodArgs,
   CoreNode,
+  creationMethodHasArguments,
   getCreationMethod,
+  getMethodCallConfigValue,
+  isCreateMethod,
   Network,
 } from "@repo/algocore";
 import {
+  AppCallParams,
   AppCallTransactionResult,
   AppCompilationResult,
   AppReference,
+  AppState,
   CreateAppParams,
 } from "@algorandfoundation/algokit-utils/types/app";
 import { useLoader, useSnackbar } from "@repo/ui";
 import { Explorer } from "@repo/algocore/src/explorer/explorer";
-import TransactionDetails from "../TransactionDetails/TransactionDetails";
 import MethodPicker from "../MethodPicker/MethodPicker";
 import {
   ABIMethod,
@@ -48,17 +58,18 @@ export function Playground({
     | null
     | (Partial<AppCompilationResult> & AppCallTransactionResult & AppReference)
   >(null);
-  const [isAppCreateTxnVisible, setAppCreateTxnVisibility] =
-    useState<boolean>(false);
 
   const [currentMethod, setCurrentMethod] = useState<ABIMethodParams | null>(
     null,
   );
+  const [executorResult, setExecutorResult] =
+    useState<AppCallTransactionResult | null>(null);
   const [executorArgs, setExecutorArgs] = useState<A_ABI_METHOD_EXECUTOR_ARG[]>(
     [],
   );
-  const [fee, setFee] = useState<string>("");
-  const [onComplete, setOnComplete] = useState<string>("");
+  const [globalStateDelta, setGlobalStateDelta] = useState<AppState | null>(
+    null,
+  );
 
   const { showLoader, hideLoader } = useLoader();
   const { showSnack, showException } = useSnackbar();
@@ -83,6 +94,85 @@ export function Playground({
   );
 
   const coreNodeInstance = new CoreNode(status, versionsCheck, genesis, health);
+
+  async function executeMethod(method: ABIMethodParams) {
+    if (!selectedNode) {
+      showSnack("Please select a network", "error");
+      return;
+    }
+    if (!selectedAccount) {
+      showSnack("Please select an account", "error");
+      return;
+    }
+
+    try {
+      const algod = new Network(selectedNode).getClient();
+      const sp = await algod.getTransactionParams().do();
+      const isCreation = isCreateMethod(method, appSpec);
+
+      if (isCreation) {
+        try {
+          showLoader("Deploying application ...");
+          const params: CreateAppParams = {
+            approvalProgram: atob(appSpec.source.approval),
+            clearStateProgram: atob(appSpec.source.clear),
+            from: mnemonicAccount(selectedAccount.mnemonic),
+            schema: {
+              globalInts: appSpec.state.global.num_uints,
+              localInts: appSpec.state.local.num_uints,
+              localByteSlices: appSpec.state.local.num_byte_slices,
+              globalByteSlices: appSpec.state.global.num_byte_slices,
+            },
+            args: {
+              methodArgs: [],
+              method: method,
+            },
+          };
+          const result = await createApp(params, algod);
+          setAppCreateResult(result);
+          hideLoader();
+        } catch (e) {
+          hideLoader();
+          showException(e);
+        }
+      } else {
+        if (!appCreateResult) {
+          showSnack(
+            "Deploy an application before invoking the methods",
+            "error",
+          );
+          return;
+        }
+        showLoader("Invoking method call ...");
+        const params: AppCallParams = {
+          appId: appCreateResult.appId,
+          callType: getMethodCallConfigValue(method, appSpec),
+          from: mnemonicAccount(selectedAccount.mnemonic),
+          populateAppCallResources: true,
+          args: {
+            method: method,
+            methodArgs: convertExecutorArgsToMethodArgs(
+              executorArgs,
+              method,
+              sp,
+              mnemonicAccount(selectedAccount.mnemonic),
+            ),
+          },
+        };
+        const result = await callApp(params, algod);
+        setExecutorResult(result);
+        if (result.confirmation?.globalStateDelta) {
+          const test = decodeAppState(result.confirmation.globalStateDelta);
+          setGlobalStateDelta(test);
+        }
+
+        hideLoader();
+      }
+    } catch (e) {
+      hideLoader();
+      showException(e);
+    }
+  }
 
   return (
     <div className={"playground-wrapper"}>
@@ -147,6 +237,14 @@ export function Playground({
                         return;
                       }
 
+                      if (creationMethodHasArguments(appSpec)) {
+                        showSnack(
+                          "This contract has a method which need few  arguments to deploy the application. please invoke that method below to deploy a new application.",
+                          "error",
+                        );
+                        return;
+                      }
+
                       try {
                         showLoader("Deploying application ...");
                         const creationMethod = getCreationMethod(appSpec);
@@ -176,7 +274,6 @@ export function Playground({
 
                         setAppCreateResult(result);
                         hideLoader();
-                        setAppCreateTxnVisibility(true);
                       } catch (e) {
                         hideLoader();
                         showException(e);
@@ -185,24 +282,11 @@ export function Playground({
                   >
                     Deploy app
                   </Button>
-                  {appCreateResult ? (
-                    <div>
-                      <TransactionDetails
-                        show={isAppCreateTxnVisible}
-                        id={appCreateResult.transaction.txID()}
-                        onClose={() => {
-                          setAppCreateTxnVisibility(false);
-                        }}
-                      ></TransactionDetails>
-                    </div>
-                  ) : (
-                    ""
-                  )}
                 </div>
               </div>
             </div>
             <div className="app-spec-body">
-              {appCreateResult ? (
+              {appCreateResult || creationMethodHasArguments(appSpec) ? (
                 <div className="method-executor">
                   <Grid container spacing={2}>
                     <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
@@ -217,6 +301,21 @@ export function Playground({
                         appSpec={appSpec}
                       ></MethodPicker>
 
+                      {currentMethod ? (
+                        <div className="app-create-info">
+                          {isCreateMethod(currentMethod, appSpec) ? (
+                            <div>
+                              This is a app create method. Executing this method
+                              will deploy a new application and set the deployed
+                              application to the current context.
+                            </div>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      ) : (
+                        ""
+                      )}
                       {currentMethod ? (
                         <div className="abi-method-args-form-wrapper">
                           <div className="abi-method-args-form-container">
@@ -404,9 +503,71 @@ export function Playground({
                             })}
 
                             <div className="abi-method-execute">
-                              <Button variant={"contained"} onClick={() => {}}>
+                              <Button
+                                variant={"contained"}
+                                onClick={() => {
+                                  executeMethod(currentMethod);
+                                }}
+                              >
                                 Execute
                               </Button>
+                            </div>
+                          </div>
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                      {executorResult ? (
+                        <div className="result-wrapper">
+                          <div className="result-container">
+                            <div className="title">Result</div>
+                            <div className="details">
+                              <div className="section">
+                                <div className="key">Return</div>
+                                <div className="value">
+                                  {executorResult?.return?.returnValue?.toString()}
+                                </div>
+                              </div>
+                              <div className="section">
+                                <div className="key">Transaction</div>
+                                <div
+                                  className="value underline hover"
+                                  onClick={() => {
+                                    new Explorer(
+                                      coreNodeInstance,
+                                    ).openTransaction(
+                                      executorResult?.transaction.txID(),
+                                    );
+                                  }}
+                                >
+                                  {executorResult?.transaction.txID()}
+                                </div>
+                              </div>
+                              <div className="section">
+                                <div className="key">Global state delta</div>
+                                {globalStateDelta ? (
+                                  <div className="value">
+                                    {Object.keys(globalStateDelta).map(
+                                      (globalStateKey) => {
+                                        return (
+                                          <div>
+                                            <div>{globalStateKey}</div>
+                                            <div>
+                                              {globalStateDelta[
+                                                globalStateKey
+                                              ]?.value.toString()}
+                                            </div>
+                                          </div>
+                                        );
+                                      },
+                                    )}
+                                  </div>
+                                ) : (
+                                  ""
+                                )}
+                              </div>
                             </div>
                           </div>
                         </div>
--- packages/algocore/src/AppSpecUtils.ts
+++ packages/algocore/src/AppSpecUtils.ts
@@ -2,7 +2,23 @@ import {
   AppSpec,
   CallConfig,
 } from "@algorandfoundation/algokit-utils/types/app-spec";
-import { ABIMethod, ABIMethodParams } from "algosdk";
+import {
+  ABIArrayDynamicType,
+  ABIArrayStaticType,
+  ABIMethod,
+  ABIMethodParams,
+  ABITupleType,
+  ABIType,
+  abiTypeIsTransaction,
+  Account,
+  algosToMicroalgos,
+  OnApplicationComplete,
+  SuggestedParams,
+  Transaction,
+  TransactionType,
+} from "algosdk";
+import { A_ABI_METHOD_EXECUTOR_ARG } from "./types";
+import { ABIAppCallArg } from "@algorandfoundation/algokit-utils/types/app";
 
 export function getCreateMethodSignature(appSpec: AppSpec): string {
   const signature = Object.keys(appSpec.hints).find(
@@ -27,6 +43,21 @@ export function getCreationMethod(
   });
 }
 
+export function hasCreationMethod(appSpec: AppSpec): boolean {
+  return getCreationMethod(appSpec) != undefined;
+}
+
+export function creationMethodHasArguments(appSpec: AppSpec): boolean {
+  if (hasCreationMethod(appSpec)) {
+    const method = getCreationMethod(appSpec);
+    if (method) {
+      return method.args.length > 0;
+    }
+  }
+
+  return false;
+}
+
 export function getMethodCallConfig(
   methodParams: ABIMethodParams,
   appSpec: AppSpec,
@@ -37,4 +68,121 @@ export function getMethodCallConfig(
   const hint = appSpec.hints[signature];
 
   return hint?.call_config;
+}
+
+export function getMethodCallConfigValue(
+  methodParams: ABIMethodParams,
+  appSpec: AppSpec,
+):
+  | "no_op"
+  | "opt_in"
+  | "close_out"
+  | "delete_application"
+  | "clear_state"
+  | OnApplicationComplete.NoOpOC
+  | OnApplicationComplete.OptInOC
+  | OnApplicationComplete.CloseOutOC
+  | OnApplicationComplete.ClearStateOC
+  | OnApplicationComplete.DeleteApplicationOC {
+  const callConfig = getMethodCallConfig(methodParams, appSpec);
+  let value: any = OnApplicationComplete.NoOpOC;
+
+  if (callConfig?.no_op) {
+    value = OnApplicationComplete.NoOpOC;
+  } else if (callConfig?.opt_in) {
+    value = OnApplicationComplete.OptInOC;
+  } else if (callConfig?.close_out) {
+    value = OnApplicationComplete.CloseOutOC;
+  } else if (callConfig?.delete_application) {
+    value = OnApplicationComplete.DeleteApplicationOC;
+  }
+
+  return value;
+}
+
+export function parseMethodArgumentValue(arg: A_ABI_METHOD_EXECUTOR_ARG): any {
+  const dataType = arg.type.toString();
+  const val = arg.value;
+
+  if (
+    arg.type instanceof ABIArrayStaticType ||
+    arg.type instanceof ABIArrayDynamicType
+  ) {
+    if (dataType.startsWith("byte")) {
+      return new Uint8Array(Buffer.from(val));
+    }
+    if (dataType.startsWith("uint")) {
+      const vals = val.split(",");
+      const intVals = vals.map((val1: any) => {
+        return BigInt(val1);
+      });
+      return intVals;
+    }
+    return val.split(",");
+  }
+  if (
+    dataType.startsWith("uint") ||
+    dataType.startsWith("ufixed") ||
+    dataType === "byte" ||
+    dataType === "asset" ||
+    dataType === "application"
+  ) {
+    return BigInt(val);
+  }
+  if (dataType === "bool") {
+    return val === "true";
+  }
+  if (arg.type instanceof ABITupleType) {
+    const encodedVal: any = [];
+    const tupleContentTypes = ABITupleType.parseTupleContent(
+      dataType.slice(1, dataType.length - 1),
+    );
+    const tupleContentValues = ABITupleType.parseTupleContent(val);
+
+    if (tupleContentTypes.length !== tupleContentValues.length) {
+      throw new Error("Tuple content length mismatch");
+    }
+
+    tupleContentTypes.forEach((tupleContentType, i) => {
+      const ti = ABIType.from(tupleContentType);
+      const arg1: A_ABI_METHOD_EXECUTOR_ARG = {
+        type: ti,
+        value: tupleContentValues[i],
+      };
+      encodedVal.push(parseMethodArgumentValue(arg1));
+    });
+
+    return encodedVal;
+  }
+
+  return val;
+}
+export function convertExecutorArgsToMethodArgs(
+  executorArgs: A_ABI_METHOD_EXECUTOR_ARG[],
+  method: ABIMethodParams,
+  sp: SuggestedParams,
+  account: Account,
+): ABIAppCallArg[] {
+  return executorArgs
+    .map((arg) => {
+      const val = parseMethodArgumentValue(arg);
+      const argType = arg.type.toString() as TransactionType;
+
+      if (abiTypeIsTransaction(argType)) {
+        let txn;
+        if (argType === TransactionType.pay) {
+          txn = new Transaction({
+            type: TransactionType.pay,
+            from: account.addr,
+            to: val.to,
+            amount: algosToMicroalgos(val.amount),
+            ...sp,
+          });
+        }
+        return txn;
+      }
+
+      return val;
+    })
+    .filter((value) => value !== undefined && value !== "" && value !== null);
 }
